b1537ad6093cdb34eefcd5a1c01677969cd5c46c
swathysuriyababu <72858078+swathysuriyababu@users.noreply.github.com>
2020-12-12 13:41:54 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,40 @@
+class Player {
+    constructor() {
+        
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        this.index = null;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
